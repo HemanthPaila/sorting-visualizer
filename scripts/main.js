31a5cc17b32e1ed0d3556f49870776c359a99c22
paila.hemanth <pailahemanth2002@gmail.com>
2022-12-06 19:41:01 +0530

    modified main method and added switch case06122022

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+function main()
+{
+    var n;
+    switch(n)
+    {
+        case 1:bubble();
+        break;
+        case 2:insertion();
+        break;
+        case 3:selection();
+        break;
+        case 4:quick();
+        break;
+        case 5:merge();
+        break;
+        case 6:heap();
+        break;
+    }
+}
