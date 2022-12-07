c79c47bea27ca0cff311f0f575fd491fef570d7a
paila.hemanth <pailahemanth2002@gmail.com>
2022-12-07 14:52:51 +0530

    added an anchor tag to note section07122022

--- scripts/bubble.js
+++ scripts/bubble.js
@@ -1,10 +1,3 @@
-document.getElementById("wcase").innerHTML="O(n)";
-document.getElementById("acase").innerHTML="Θ(n^2)";
-document.getElementById("bcase").innerHTML="Ω(n^2)";
-document.getElementById("space").innerHTML="O(1)";
-
-
-
 function bubble_sort()
 {
     for(var i=0;i<arr.length-1;i++)
