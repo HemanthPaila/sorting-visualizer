4e4f3425d2c9ea73d047fb46d6d2f0354c4f8a08
paila.hemanth <pailahemanth2002@gmail.com>
2023-11-25 15:06:44 +0530

    updated

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-var express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+// import { dirname } from 'path';
+import path from 'path';
+const __dirname = path.resolve();
+
+// var express = require("express");
 var app = express();
-var bodyParser = require("body-parser");
-const https = require("https");
+// var bodyParser = require("body-parser");
+// const https = require("https");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, 'public')));
 const port = 3001;
 // var mysql = require("mysql2");
 // const { json } = require("body-parser");
@@ -17,10 +24,10 @@ const port = 3001;
 app.get("/", function (req, res) {
   res.render("index");
 });
-app.post("/update", function (req, res) {
-  var person = {
-    user_name: req.body.data1,
-  };
+// app.post("/update", function (req, res) {
+//   var person = {
+//     user_name: req.body.data1,
+//   };
   // https.get('https://api.github.com/users/HemanthPaila', function(response){
   //   response.on('data', function(data){
   //     var json=data.toString();
@@ -28,16 +35,16 @@ app.post("/update", function (req, res) {
   //     console.log(gitData);
   //   });
   // });
-  var end_result = connection.query(
-    "INSERT INTO data SET ?",
-    person,
-    function (err, result) {
-      if (err) throw err;
+  // var end_result = connection.query(
+  //   "INSERT INTO data SET ?",
+  //   person,
+  //   function (err, result) {
+  //     if (err) throw err;
       // console.log(result);
-    }
-  );
-  res.render("index");
-});
+//     }
+//   );
+//   res.render("index");
+// });
 app.listen(port, function () {
   console.log(`server is running at port number ${port}`);
 });
